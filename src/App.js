7f3002297defc4c183f18e00660843c75ecc5ed2
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-10-10 15:13:02 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import * as React from "react";
 import "./App.css";
 
-/* Reusable dropdown component */
-
 const App = () => {
-  const [open, setOpen] = React.useState(false);
+  //Move this to the dropdown component
+  //const [open, setOpen] = React.useState(false);
 
-  const handleOpen = () => {
-    setOpen(!open);
-  };
+   //Move this to the dropdown component
+  // const handleOpen = () => {
+  //   setOpen(!open);
+  // };
 
-  /* The dropdown's menu should float above the other HTML elements. 
-  What's missing are event handlers for each button in the dropdown's menu. 
-  For each event handler, you can perform something specific like opening a 
-  dialog for example, while the handler also has to close the dropdown menu eventually:*/
   const handleMenuOne = () => {
-    // do something
-    setOpen(false);
+    console.log('clicked one');
   };
 
   const handleMenuTwo = () => {
-    // do something
-    setOpen(false);
+    console.log('clicked two');
   };
 
   return (
     <Dropdown
-      open={open}
-      trigger={<button onClick={handleOpen}>Dropdown</button>}
+      trigger={<button>Dropdown</button>}
       menu={[
         <button onClick={handleMenuOne}>Menu 1</button>,
         <button onClick={handleMenuTwo}>Menu 2</button>,
@@ -37,15 +30,53 @@ const App = () => {
 };
 
 
-//Create a dropdown control
-const Dropdown = ({ open, trigger, menu }) => {
+//Create a dropdown control - has two props
+//open - prop to signify open or close
+//trigger - prop is for dropdown to render the menu as list
+//menu - prop containing the "buttons" for menu1 and menu2
+
+//However there is still logic of the dropdown component 
+//sitting in its parent component App.js.
+
+//When instantiating multiple dropdown components, this will 
+//become repetitive logic in each parent component.
+
+//So the next step shows how to elegantly move all repetitive 
+//implementation details such as:
+//   open state
+//   const handleOpen = () => {
+//   const [open, setOpen] = React.useState(false);
+//into the dropdown component by using 
+//React's cloneElement API:
+
+const Dropdown = ({ trigger, menu }) => {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="dropdown">
-      {trigger}
+       {React.cloneElement(trigger, {
+        onClick: handleOpen,
+      })}   
+
+      {/* React's cloneElement API allows us to attach props to the passed trigger element (here: opening/closing the dropdown, because it toggles the open state within the dropdown component). */}
       {open ? (
         <ul className="menu">
           {menu.map((menuItem, index) => (
-            <li key={index} className="menu-item">{menuItem}</li>
+            <li key={index} className="menu-item">{menuItem}
+               {React.cloneElement(menuItem, {
+                onClick: () => {
+                  // Furthermore, the high-level API allows us to close the dropdown once a menu item in a dropdown is clicked while still preserving its native implementation (here: menuItem.props.onClick).
+                  menuItem.props.onClick();
+                  setOpen(false);
+                },
+              })}
+            </li>
+
           ))}
         </ul>
       ) : null}
@@ -54,3 +85,12 @@ const Dropdown = ({ open, trigger, menu }) => {
 };
 
 export default App;
+
+/* 
+ The reusable dropdown component is finished. What's missing is 
+ the implementation detail to close the dropdown if a user 
+ clicks outside of it. To do this go to: 
+
+     React Hook: Detect Click outside of Component
+     https://www.robinwieruch.de/react-hook-detect-click-outside-component/
+*/
